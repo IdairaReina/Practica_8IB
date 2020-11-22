1f86fb5adbc5d438f147e25daf758cf42812de2c
IdairaReina <alu0101398495@ull.edu.es>
2020-11-22 10:40:28 +0000

    chances

--- difference_of_squares/difference_of_squares.cc
+++ difference_of_squares/difference_of_squares.cc
@@ -1,7 +1,23 @@
 #include <iostream>
+#include "difference_of_squares.h"
 using namespace std;
- int main()
-{
+
+
+void Usage(int argc, char *argv[]) {
+	if (argc !=2){
+		cout << argv[0] << "Falta un número natural como parámetro" << endl;
+		cout << "Pruebe " << argv[0] << "--help para más información" << endl;
+		exit(EXIT_SUCCESS);
+	}
+	string parameter{argv[1]};
+	if (parameter == "--help"){
+		cout << KHelpText << endl;
+		exit(EXIT_SUCCESS);
+	}
+}
+
+
+ int main(int argc, char* argv[]){
  float i,suma,suma2,n;
  suma2=0;
  suma=0;
---
+++ difference_of_squares/difference_of_squares.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+const std::string KHelpText = "Este programa toma un número natural N e imprime en pantalla la diferencia entre el cuadrado de la suma de los primeros N números naturales y la suma de los cuadrados de esos mismos números";
+
+void Usage(int argc, char *argv[]);
